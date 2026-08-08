cc6d4b5b13435ffd2e6bb1619628d4e42dd8d3a5
Mykhailo Loskutov <m1i9h7a8@gmail.com>
2026-08-08 10:59:48 +0300

    Виконані зміни

--- lesson_04_str/home/homework_04.py
+++ lesson_04_str/home/homework_04.py
@@ -21,35 +21,65 @@ rolling in wealth."""
 
 # УВАГА! Перезаписуйте вміст змінної adwentures_of_tom_sawer у завданнях 01-03
 
+adwentures_of_tom_sawer_copy_orig = adwentures_of_tom_sawer
+
+
 # task 01 ==
 """ Дані у строці adwentures_of_tom_sawer розбиті випадковим чином, через помилку.
 треба замінити кінець абзацу на пробіл .replace("\n", " ")"""
 
+adwentures_of_tom_sawer = adwentures_of_tom_sawer.rstrip("\n").replace("\n", " ")
+print(adwentures_of_tom_sawer)
+
+
 # task 02 ==
 """ Замініть .... на пробіл
 """
-
+adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace("....", " ")
+print(adwentures_of_tom_sawer)
 
 # task 03 ==
 """ Зробіть так, щоб у тексті було не більше одного пробілу між словами.
 """
+adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace("   ", " ")
+adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace("  ", " ")
+print(adwentures_of_tom_sawer)
 
 
 # task 04
 """ Виведіть, скількі разів у тексті зустрічається літера "h"
 """
-
+print(adwentures_of_tom_sawer.count("h"))
 
 # task 05
 """ Виведіть, скільки слів у тексті починається з Великої літери?
 підказка - порахувати кожну велику літеру напр, .count("A") і їх сумму
 """
-
+print(
+    adwentures_of_tom_sawer.count("A") + adwentures_of_tom_sawer.count("B") +
+    adwentures_of_tom_sawer.count("C") + adwentures_of_tom_sawer.count("D") +
+    adwentures_of_tom_sawer.count("E") + adwentures_of_tom_sawer.count("F") +
+    adwentures_of_tom_sawer.count("G") + adwentures_of_tom_sawer.count("H") +
+    adwentures_of_tom_sawer.count("I") + adwentures_of_tom_sawer.count("J") +
+    adwentures_of_tom_sawer.count("K") + adwentures_of_tom_sawer.count("L") +
+    adwentures_of_tom_sawer.count("M") + adwentures_of_tom_sawer.count("N") +
+    adwentures_of_tom_sawer.count("O") + adwentures_of_tom_sawer.count("P") +
+    adwentures_of_tom_sawer.count("Q") + adwentures_of_tom_sawer.count("R") +
+    adwentures_of_tom_sawer.count("S") + adwentures_of_tom_sawer.count("T") +
+    adwentures_of_tom_sawer.count("U") + adwentures_of_tom_sawer.count("V") +
+    adwentures_of_tom_sawer.count("W") + adwentures_of_tom_sawer.count("X") +
+    adwentures_of_tom_sawer.count("Y") + adwentures_of_tom_sawer.count("Z")
+)
 
 # task 06
 """ Виведіть позицію, на якій слово Tom зустрічається вдруге
 """
 
+first_tom = adwentures_of_tom_sawer.find("Tom")
+
+second_tom = adwentures_of_tom_sawer.find("Tom", first_tom + 1)
+
+print(second_tom)
 
 # task 07
 """ Розділіть змінну adwentures_of_tom_sawer по кінцю речення.
@@ -57,17 +87,25 @@ rolling in wealth."""
 """
 adwentures_of_tom_sawer_sentences = None
 
+adwentures_of_tom_sawer_sentences = adwentures_of_tom_sawer.split(". ")
+print(adwentures_of_tom_sawer_sentences)
+
 # task 08
 """ Виведіть четверте речення з adwentures_of_tom_sawer_sentences.
 Перетворіть рядок у нижній регістр.
 """
-
+print(adwentures_of_tom_sawer_sentences[3].lower())
 
 # task 09
 """ Перевірте чи починається якесь речення з "By the time".
 """
-
+find_by_the_time = any(sentence.startswith("By the time") for sentence in adwentures_of_tom_sawer_sentences)
+print(find_by_the_time)
 
 # task 10
 """ Виведіть кількість слів останнього речення з adwentures_of_tom_sawer_sentences.
 """
+
+last_sentence = adwentures_of_tom_sawer_sentences[-1]
+word_count = len(last_sentence.split())
+print(word_count)
