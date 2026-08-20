08e2d3de107aee483209d62b6e52ca4ef5d4ed96
Mykhailo Loskutov <m1i9h7a8@gmail.com>
2026-08-20 17:06:56 +0300

    homework8 competd

--- lesson_08_pathlib/homework_08.py
+++ lesson_08_pathlib/homework_08.py
@@ -8,6 +8,9 @@
    ```
 """
 # coding here
+from pathlib import Path
+with open("hello.txt", "w", encoding="utf-8") as f:
+    f.write("Hello, Python!\n")
 
 
 
@@ -16,6 +19,9 @@
    Відкрий файл `hello.txt` і виведи його вміст на екран.
 """
 # coding here
+with open("hello.txt", "r", encoding="utf-8") as f:
+   content = f.read()
+print(content)
 
 """   
 3. **Дозапис у файл**
@@ -26,18 +32,27 @@
    ```
 """
 # coding here
+with open("hello.txt", "a", encoding="utf-8") as f:
+    f.write("Learning file operations.\n")
+    
+
 
 """
 4. **Читання кількох рядків**
    Виведи всі рядки з файлу `hello.txt` по одному рядку (без додаткових символів `\n`).
 """
 # coding here
+with open("hello.txt", "r", encoding="utf-8") as f:
+    for line in f:
+        print(line.strip())
 
 """
 5. **Підрахунок символів**
    Прочитай файл `hello.txt` і виведи кількість символів у ньому.
 """
 # coding here
+with open("hello.txt", "r", encoding="utf-8") as f:
+    print(f"Кількість символів у файлі: {len(f.read())}")
 
 """
 6. **Створення папки**
@@ -48,18 +63,45 @@
    ```
 """
 # coding here
+new_directory = Path(r"C:\Users\Misha2\Desktop\Course\qal_rc_pd_g1\lesson_08_pathlib\data")
+new_directory.mkdir(parents=True, exist_ok=True)
+file_path = new_directory / "notes.txt"
+with open(file_path, "w", encoding="utf-8") as f:
+    f.write("My first note.\n")
+
+
 
 """
 7. **Список файлів у папці**
    Виведи на екран список усіх файлів у папці `data`.
 """
 # coding here
+files = [f for f in new_directory.iterdir() if f.is_file()]
+
+# Виведення списку всіх файлів
+print("Список всіх файлів:")
+for file in files:
+    print(file)
 
 """
 8. **Копіювання вмісту**
    Прочитай вміст файлу `notes.txt` і запиши його у файл `copy.txt` (у тій же папці `data`).
 """
 # coding here
+
+source_file = new_directory / "notes.txt"
+destination_file = new_directory / "copy.txt"
+
+with open(source_file, "r", encoding="utf-8") as f:
+    content = f.read()
+    print(f"Вміст файлу notes.txt: {content}")
+
+with open(destination_file, "w", encoding="utf-8") as f:
+      f.write(content + "\n")
+with open(destination_file, "r", encoding="utf-8") as f:
+    copy_content = f.read()
+print(f"Вміст файлу copy.txt: {copy_content}")
+
 
 """
 9. **Об’єднання файлів**
@@ -68,9 +110,40 @@
 """
 # coding here
 
+file_path = new_directory / "a.txt"
+with open(file_path, "w", encoding="utf-8") as f:
+    f.write("My note in file a.txt.\n")
+file_path = new_directory / "b.txt"
+with open(file_path, "w", encoding="utf-8") as f:
+    f.write("My note in file b.txt.\n")
+
+file_ab_path = new_directory / "ab.txt"
+
+with open(new_directory / "a.txt", "r", encoding="utf-8") as f:
+    text_a = f.read()
+
+with open(new_directory / "b.txt", "r", encoding="utf-8") as f:
+    text_b = f.read()
+
+with open(file_ab_path, "w", encoding="utf-8") as f:
+    f.write(text_a + text_b)
+with open(file_ab_path, "r", encoding="utf-8") as f:
+    text_a_b = f.read()
+
+print("Файли 'a.txt' та 'b.txt' успішно об'єднані у файл 'ab.txt'!") 
+print(f"Вміст файлу ab.txt: \n{text_a_b}")
+
 """
 10. **Пошук слова у файлі**
     У файлі `notes.txt` перевір, чи є слово `"note"`.
     Якщо є — виведи `"Знайдено"`, інакше `"Не знайдено"`.
 """
 # coding here
+
+file_to_search = new_directory / "notes.txt"
+with open(file_to_search, "r", encoding="utf-8") as f:
+    file_content = f.read()
+if "note" in file_content:
+    print("Знайдено")
+else:
+    print("Не знайдено")
